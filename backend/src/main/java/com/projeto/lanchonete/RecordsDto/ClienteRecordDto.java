464a580bb805e1a4123787b9fb27eb7abda02a8a
devguina <guisouzaskill@gmail.com>
2024-10-01 15:45:09 -0300

    ajustes finos

--- backend/src/main/java/com/projeto/lanchonete/RecordsDto/ClienteRecordDto.java
+++ backend/src/main/java/com/projeto/lanchonete/RecordsDto/ClienteRecordDto.java
@@ -1,5 +1,7 @@
 package com.projeto.lanchonete.RecordsDto;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -7,7 +9,8 @@ import jakarta.validation.constraints.Size;
 public record ClienteRecordDto(@NotBlank @Size(max =255 ) String nomeCliente,
                                @NotBlank @Size(max = 255) String ruaCliente,
                                @NotBlank @Size(max = 100) String bairroCliente,
+                               @NotBlank @Size(max = 100) String cidadeCliente,
                                @NotNull int numeroCasaCliente,
                                @NotBlank @Size(max = 20 ) String numeroTelefoneCliente,
-                               @NotBlank @Size(max = 100) String emailCliente) {
+                               @NotBlank @Valid @Email @Size(max = 100) String emailCliente) {
 }
